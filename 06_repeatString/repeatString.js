bc88edfdf3c34dee45674b811a96dc3e566ac7d3
Barbara <barbara-albuquerque@outlook.com>
2025-09-24 11:38:19 -0300

    resolução do exercício 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let aux = ""
+    for (let index = 0; index < num; index++) {
+        aux = aux.concat(string)
+    }
+    return aux;
 };
 
 // Do not edit below this line
