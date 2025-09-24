d7409e8e5c3ec45429aeb155d2a098ef475345d1
Barbara <barbara-albuquerque@outlook.com>
2025-09-24 11:47:30 -0300

    resolução do exercicio 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let aux = ""
+    let aux_leng = string.length-1
+    for (let index = aux_leng; index >= 0; index--) {
+        aux = aux.concat(string[index]);
+        
+    }
+    return aux;
 };
 
 // Do not edit below this line
